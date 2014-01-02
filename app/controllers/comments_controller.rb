2f1e1763d309c06ed5a36d5f8f108e8ebfe923dc
samgooi4189 <samgooi4189@UMachine.(none)>
2014-01-01 22:02:43 -0800

    Added create function for comment and showing comment after created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+	def create
+		@post = Post.find(params[:id])
+		@comment = @post.comments.create(post_params)
+		redirect_to post_path(@post)
+	end
+
+	private 
+		def post_params
+			params.require(:comment).permit(:commenter, :body)
+		end
 end
