bd46cfd029ded8fb51c0f8202fc6824b3bb8dda6
samgooi4189 <samgooi4189@UMachine.(none)>
2014-01-06 22:00:13 -0800

    Added a few tests for post controller

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,4 +4,21 @@ class PostsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+	test "Creating new post" do 
+		assert Post.new
+	end
+
+	test "Destroy post" do
+		p = Post.new
+		assert p.destroy
+	end
+
+	test "Update post" do
+		p = Post.new 
+		p.update_attributes(:title=>'miao')
+		
+		assert_match('miao', p.title, "The post should be updated.")
+	end
+
 end
