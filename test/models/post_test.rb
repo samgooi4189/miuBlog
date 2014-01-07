130e5d136145b4d71f4efaaf318ad47bc67a6baa
samgooi4189 <samgooi4189@UMachine.(none)>
2014-01-06 23:03:37 -0800

    Added test dynamic test data and test for model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,10 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+	test "Checking fixtures executability" do
+		assert_same(1002, Post.all.count, "The total posts should be 102, but the count is #{Post.all.count}")
+	end
+
 end
